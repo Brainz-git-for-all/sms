d0970409e1e1c7d20f586eedaef2c3412058052d
Brainz-git-for-all <brainzgit@gmail.com>
2026-07-23 13:26:41 +0000

    refactor(RegisterRequest) added notblank and size annotations

--- Backend/sms-backend/sms-backend/src/main/java/com/brainz/sms_backend/auth/RegisterRequest.java
+++ Backend/sms-backend/sms-backend/src/main/java/com/brainz/sms_backend/auth/RegisterRequest.java
@@ -7,6 +7,8 @@ package com.brainz.sms_backend.auth;
 //   String password  — @NotBlank, @Size(min=6)
 //   Role   role      — @NotNull  (uses your Role enum)
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +20,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RegisterRequest {
     // TODO: add annotations and fields
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String username;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
     private Role role;
 }
